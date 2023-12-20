88f95c76fc67349fe53dda0ee9701974e06fd17b
hadarza <hadar.zaguri@gmail.com>
2023-12-21 01:19:48 +0200

    set up main activity & connect to log in

--- app/src/main/java/com/android/skillsync/MainActivity.kt
+++ app/src/main/java/com/android/skillsync/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.android.skillsync
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.android.skillsync.R
 
 class MainActivity : AppCompatActivity() {
+
+    var logInButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logInButton = findViewById(R.id.button_sign_in)
+        logInButton?.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+        }
+
     }
 }
