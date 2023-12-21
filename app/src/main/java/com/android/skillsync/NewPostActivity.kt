29672bddafd29c8a90958781e29c9b2733046a61
hadarza <hadar.zaguri@gmail.com>
2023-12-21 20:06:59 +0200

    set up add post page - nofar

---
+++ app/src/main/java/com/android/skillsync/NewPostActivity.kt
@@ -0,0 +1,11 @@
+package com.android.skillsync
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_post)
+    }
+}
