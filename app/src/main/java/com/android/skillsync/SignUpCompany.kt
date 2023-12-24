ec163eafc084c13fef8c7183d3fda70af7b1d620
hadarza <hadar.zaguri@gmail.com>
2023-12-24 23:28:23 +0200

    set up initial auto complete + change custom_input password/text/email

--- app/src/main/java/com/android/skillsync/SignUpCompany.kt
+++ app/src/main/java/com/android/skillsync/SignUpCompany.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import com.android.skillsync.databinding.ActivitySignUpCompanyBinding
 import com.android.skillsync.models.Comapny
@@ -11,15 +13,12 @@ import com.android.skillsync.models.Type
 import com.android.skillsync.models.UserType
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.database
 import com.google.firebase.firestore.firestore
 
 class SignUpCompany : AppCompatActivity() {
 
     private lateinit var binding:ActivitySignUpCompanyBinding
     private lateinit var firebaseAuth: FirebaseAuth
-//    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +26,12 @@ class SignUpCompany : AppCompatActivity() {
         binding  = ActivitySignUpCompanyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        addAutoSuggestionsCompaniesList()
+
         firebaseAuth = FirebaseAuth.getInstance()
 
-
         binding.signUpCompany.setOnClickListener{
-            val companyName = binding.companyName.text.toString()
+            val companyName = binding.companySuggestion.text.toString()
             val email = binding.email.text.toString()
             val password = binding.password.text.toString()
             if((companyName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()))
@@ -43,6 +43,33 @@ class SignUpCompany : AppCompatActivity() {
                     Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+    }
+
+    fun addAutoSuggestionsCompaniesList() {
+        val autoCompany: AutoCompleteTextView = findViewById(R.id.companySuggestion)
+
+        val listSuggestionsCompanies = ArrayList<String>()
+        listSuggestionsCompanies.add("wix")
+        listSuggestionsCompanies.add("monday")
+        listSuggestionsCompanies.add("mongo")
+        listSuggestionsCompanies.add("Mota Group")
+        listSuggestionsCompanies.add("mobile app")
+        listSuggestionsCompanies.add("bbb")
+        listSuggestionsCompanies.add("burgers")
+        listSuggestionsCompanies.add("playtika")
+        listSuggestionsCompanies.add("facebook")
+        listSuggestionsCompanies.add("microsoft")
+        listSuggestionsCompanies.add("unity")
+        listSuggestionsCompanies.add("google")
+        listSuggestionsCompanies.add("amazon")
+        listSuggestionsCompanies.add("apple")
+
+        val companiesAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listSuggestionsCompanies)
+
+        autoCompany.setAdapter(companiesAdapter)
+        autoCompany.setOnItemClickListener{ adapterView, view, i, l ->
+            Toast.makeText(this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_LONG).show()
         }
     }
 
