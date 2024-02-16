8976c9b87d63727374a1283d1e34c5ed76fa3478
hadarza <hadar.zaguri@gmail.com>
2024-02-16 02:53:47 +0200

    set up new api key

--- app/src/main/java/com/android/skillsync/services/PlacesApiService.kt
+++ app/src/main/java/com/android/skillsync/services/PlacesApiService.kt
@@ -9,7 +9,7 @@ import retrofit.http.POST
 interface PlacesApiService {
     @Headers(
         "Content-Type: application/json",
-        "X-API-KEY: c39a6e95c0723e63321df9caceaf48a1531c6a8d"
+        "X-API-KEY: 6ad2bb4f22fca025e6fad9dae09f951eb1c6ab71"
     )
     @POST("places")
     fun getPlaces(@Body body: Map<String, Any>): Call<PlacesResponse>
