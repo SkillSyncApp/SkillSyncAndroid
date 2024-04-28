56aa66a8a409a916a513f9446e4a833c5c371c2d
Hadar Zaguri <48456834+hadarza@users.noreply.github.com>
2024-04-29 01:21:15 +0300

    on post click - show owner profile (#77)
    Co-authored-by: hadar zaguri <hadarza@wix.com>

--- app/src/main/java/com/android/skillsync/adapters/PostAdapter.kt
+++ app/src/main/java/com/android/skillsync/adapters/PostAdapter.kt
@@ -1,28 +1,50 @@
 package com.android.skillsync.adapters
 
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.android.skillsync.FeedFragment
 import com.android.skillsync.R
 import com.android.skillsync.models.Post.Post
 import com.squareup.picasso.Picasso
 
-class PostAdapter(var posts: MutableList<Post>)
-    : RecyclerView.Adapter<PostAdapter.PostHolder>() {
+class PostAdapter(var posts: MutableList<Post>, var isFromFeed: Boolean) : RecyclerView.Adapter<PostAdapter.PostHolder>() {
 
-    class PostHolder(itemView:View): RecyclerView.ViewHolder(itemView) { // check if we need to split to a different file - check tal github
+    var listener: FeedFragment.OnPostClickListener? = null
+
+    fun setOnPostClickListener(listener: FeedFragment.OnPostClickListener) {
+        this.listener = listener
+    }
+
+    class PostHolder(
+        itemView: View,
+        private val posts: List<Post>,
+        val listener: FeedFragment.OnPostClickListener?,
+        isFromFeed: Boolean
+    ) : RecyclerView.ViewHolder(itemView) {
         val ownerNameLabel: TextView = itemView.findViewById(R.id.ownerName)
         val contentLabel: TextView = itemView.findViewById(R.id.content)
         val image = itemView.findViewById<ImageView>(R.id.imagePost)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION && isFromFeed) {
+                    listener?.onPostClicked(posts[position])
+                    Log.d("onClickPost", posts[position].ownerId)
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.post_item, parent, false)
-        return PostHolder(view)
+        return PostHolder(view, posts, listener, isFromFeed)
     }
 
     override fun getItemCount(): Int {
